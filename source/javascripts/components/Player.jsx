96c39c0f7cb2877e5ba8088407a4b6da9e3e427b
Mathieu Débit <mathieu.debit@gmail.com>
2015-10-19 11:26:06 +0200

    `Player.jsx` component

---
+++ source/javascripts/components/Player.jsx
@@ -0,0 +1,35 @@
+/*jshint esnext: true */
+
+class Player extends React.Component {
+  render() {
+    var player = this.props.player;
+
+    return (
+      <section id='player' className={'player-' + this.props.team}>
+        <div className='avatar'>
+          <img src={player.avatar} />
+          <span>{player.name}</span>
+        </div>
+
+        <div className='twitter'>
+          <span>@{player.screenname}</span>
+        </div>
+
+        <span className='points'>{this.props.playerCount}</span>
+
+        <p className='button'>Vote ou RT avec #TaFeuille{this.team()}</p>
+      </section>
+    );
+  }
+
+  team(){
+    var team;
+    if (this.props.team == 'red'){
+      team = 'rouge';
+    };
+    if (this.props.team == 'blue') {
+      team = 'bleu'
+    };
+    return team;
+  }
+}
