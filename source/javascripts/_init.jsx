b950ee6cbcf9aebb656f86648339478594c507f6
Mathieu Débit <mathieu.debit@gmail.com>
2015-10-19 11:29:09 +0200

    Adding two players

--- source/javascripts/_init.jsx
+++ source/javascripts/_init.jsx
@@ -1,4 +1,17 @@
+var players = [
+  {
+    avatar: 'https://tristanwright.files.wordpress.com/2010/09/self-portrait-gif.gif',
+    name: 'Jean Gueux',
+    screenname: 'xX-Dark_Jean-Xx'
+  },
+  {
+    avatar: 'https://tristanwright.files.wordpress.com/2010/09/self-portrait-gif.gif',
+    name: 'Goerges',
+    screenname: 'goerge-du-64'
+  }
+]
+
 React.render(
-  <App />,
+  <App players={players}/>,
   document.getElementById('app')
 );
