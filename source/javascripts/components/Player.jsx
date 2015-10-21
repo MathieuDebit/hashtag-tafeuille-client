dc66ce1dd572ef181349827ca7738c59f5424ea4
Valentin Galmand <Valentin@MacBook-Pro-de-Valentin.local>
2015-10-21 23:45:02 +0200

    design-v1.0
    Sass compile doesn’t work

--- source/javascripts/components/Player.jsx
+++ source/javascripts/components/Player.jsx
@@ -5,18 +5,25 @@ class Player extends React.Component {
     var player = this.props.player;
 
     return (
-      <section id='player' className={'player-' + this.props.team}>
-        <div className='avatar'>
-          <img src={player.avatar} />
-          <span>{player.name}</span>
-        </div>
+      // <section id='player' className={'player-' + this.props.team}>
+      //   <div className='avatar'>
+      //     <img src={player.avatar} />
+      //     <span>{player.name}</span>
+      //   </div>
+      //
+      //   <div className='twitter'>
+      //     <span>@{player.screenname}</span>
+      //   </div>
+      //
+      //   <p className='button'>Vote ou RT avec #TaFeuille{this.team()}</p>
+      // </section>
 
-        <div className='twitter'>
-          <span>@{player.screenname}</span>
-        </div>
-
-        <p className='button'>Vote ou RT avec #TaFeuille{this.team()}</p>
-      </section>
+      <div class="profile" className={'player-' + this.props.team}><img src={player.avatar} class="avatar"/>
+    <div class="infos">
+      <p class="name">{player.name}</p>
+      <p class="twitter">@{player.screenname}</p>
+    </div>
+  </div>
     );
   }
 
